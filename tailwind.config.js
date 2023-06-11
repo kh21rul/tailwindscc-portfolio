23ca2031c19621046e2fc857d37b9222707286a0
Khairul Aqram <khairulaqram21@gmail.com>
2023-06-11 07:15:22 +0700

    add file

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#6b21a8",
+        dark: "#0f172a",
+      },
+    },
+  },
+  plugins: [],
+};
