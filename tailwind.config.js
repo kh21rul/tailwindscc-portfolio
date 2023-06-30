110c0488dec768064db24060330a42c13dc78df7
Khairul Aqram <khairulaqram21@gmail.com>
2023-06-30 17:19:01 +0700

    pembuatan about section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,18 @@
 module.exports = {
   content: ["index.html"],
   theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
     extend: {
       colors: {
         primary: "#6b21a8",
+        secondary: "#475569",
         dark: "#0f172a",
+      },
+      screens: {
+        '2xl': '1320px',
       },
     },
   },
